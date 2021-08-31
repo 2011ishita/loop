aa92bd3e21fe1032b5e767164bea2d531919a0e9
2011ishita <89540470+2011ishita@users.noreply.github.com>
2021-08-31 23:05:36 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int rows;
+cout<<"\nEnter the numbers of rows you want to be in Floyd's triangle: ";
+cin>>"\nFloyd's Triangle \n";
+int k=1 ;
+for(int i=1;i<rows; ++i)
+{
+	for(int j=1; j<=i; ++j)
+   {
+   	  cout<<k<<" ";
+   	++k;
+	}
+	cout<<"\n"
+}
+reyurn 0;
+}
+
+		
+
+
+
